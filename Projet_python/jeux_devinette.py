54127da31ecd5de76061a7fc7fe822261cca458b
MisterAwlaan <nidri@guardiaschool.fr>
2025-01-22 20:06:44 +0100

    ajout de mon premier projet

---
+++ Projet_python/jeux_devinette.py
@@ -0,0 +1,22 @@
+import random 
+bot = random.randint(0,10)
+nb_vie = 3
+joueur = int(input("choisi un nombre entre 0 et 10 : "))
+
+
+while nb_vie != 0 :
+    if joueur > bot : 
+        print("tu es trop grand ")
+        nb_vie -= 1
+        joueur = int(input("choisi un nombre entre 0 et 10 : "))
+
+    elif joueur < bot : 
+        print("tu es trop petit")
+        nb_vie -= 1
+        joueur = int(input("choisi un nombre entre 0 et 10 : "))
+
+    elif joueur == bot : 
+        print("Félication tu as réussie")
+
+if nb_vie == 0 : 
+    print("tu as perdue !!")
