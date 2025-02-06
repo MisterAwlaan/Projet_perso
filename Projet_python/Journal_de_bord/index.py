268166d8de788b0c2c063e4f9755e2042d9a3685
MisterAwlaan <nidri@guardiaschool.fr>
2025-02-06 18:52:03 +0100

    modification de petite du jeux des devinettes et ajout de la fonction formulaire_pour_afficher()

--- Projet_python/Journal_de_bord/index.py
+++ Projet_python/Journal_de_bord/index.py
@@ -24,7 +24,7 @@ def menu():
     root.mainloop()
 
 def ajouter_text(date,text):
-    text = f"{date},{text},;\n"
+    text = f"{date},{text},;"
     with open("journal.txt","a",encoding="utf-8") as fichier :
         fichier.write(text)
     messagebox.showinfo("Succès","Votre texte est bien enregistré")
@@ -34,19 +34,15 @@ def formulaire_ajout_text():
     root.title("Ajouter un texte")
     root.geometry("400x400")
     root.config(background="#7366df")
-    
     F = font.Font(size=15)
-    
     label_date = tk.Label(root,text="Date(jj/mm/aaaa)",bg="#7366df",fg="white")
     zone_date = tk.Entry(root,bd=1)
     label_texte = tk.Label(root,text="Votre Récit",bg="#7366df",fg="white")
     zone_texte = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=40, height=10)
     bouton_enregistrer = tk.Button(root,text="Enregistrer",bg="#cd2bf8",fg="white",command=lambda:ajouter_text(zone_date.get(),zone_texte.get("1.0",tk.END)))
     bouton_fermer = tk.Button(root,text="Fermer",bg="red",fg="white",command=lambda:fermer(root))
-    
     label_date['font'] = F
     label_texte['font'] = F
-
     zone_date.place(x=130,y=50)
     label_date.place(x=110,y=20)
     label_texte.place(x=130,y=80)
@@ -54,7 +50,7 @@ def formulaire_ajout_text():
     bouton_enregistrer.place(x=150,y=310)
     bouton_fermer.place(x=160,y=345)
     root.mainloop()
-    
+
 def fermer(root):
     return root.destroy()
 
@@ -65,18 +61,36 @@ def str_en_liste(text):
         x.append(i.split(",")) 
     return x
     
-
 def afficher(date):
+    root = tk.Tk()
+    root.geometry("400x400")
+    root.title("Affichage")
+    root.config(background="#7366df")    
+    liste_secondaire = []
     with open("journal.txt","r") as fichier : 
         text = fichier.read()
         liste = str_en_liste(text)
     for i in liste : 
-        if str(date) == i :
-            separateur = ","
-            x = separateur.join()
+        if i[0] == date :
+            liste_secondaire.append(i)
+    for i in liste_secondaire : 
+        x = ",".join(i)
+    resultat = tk.Label(root,text=x,bg="white",fg="black")
+    Fermer = tk.Button(root,text="Fermer",bg="red",fg="white",command=lambda:fermer(root))
+    Fermer.pack()
+    resultat.pack()
+    root.mainloop()
 
 
-with open("journal.txt","r") as fichier : 
-    txt = fichier.read()
-    liste = str_en_liste(txt)
-    print(liste)
+def formulaire_pour_afficher():
+    root = tk.Tk()
+    root.geometry("400x400")
+    root.title("Formulaire pour envoyer la date")
+    date_label = tk.Label(root,text="Date(jj/mm/aaaa)")
+    zone_de_texte = tk.Entry(root,bd=5)
+    date_label.place(x=0,y=100)
+    zone_de_texte.place(x=100,y=100)
+    root.mainloop()
+
+
+formulaire_pour_afficher()
