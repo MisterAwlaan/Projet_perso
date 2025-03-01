a98410e4d7ab01a7e53eff00f9a1aef7096837d4
MisterAwlaan <nidri@guardiaschool.fr>
2025-03-01 12:55:40 +0100

    Ajout de la fonction verif_horizon

--- Projet_python/Puissance_4/index.py
+++ Projet_python/Puissance_4/index.py
@@ -31,9 +31,28 @@ def verif_verti():
     if a[0][3] == 2 and a[1][3] == 2 and a[2][3] == 2 and a[3][3] == 2 :
         return True 
 
-    
-
 def verif_horizon():
+    if a[0][0] == 1 and a[0][1] == 1 and a[0][2] == 1 and a[0][3] == 1  :
+        return True
+    if a[0][0] == 2 and a[0][1] == 2 and a[0][2] == 2 and a[0][3] == 2  :
+        return True
+
+    if a[1][0] == 1 and a[1][1] == 1 and a[1][2] == 1  and a[1][3]== 1:
+        return True 
+    if a[1][0] == 2 and a[1][1] == 2 and a[1][2] == 2  and a[1][3]== 2:
+        return True
+
+    if a[2][0] == 1 and a[2][1] == 1 and a[2][2] == 1 and a[2][3] == 1 :
+        return True
+    if a[2][0] == 2 and a[2][1] == 2 and a[2][2] == 2 and a[2][3] == 2 :
+        return True
+
+    if a[3][0] == 1 and a[3][1] == 1 and a[3][2] == 1 and a[3][3] == 1 :
+        return True
+    if a[3][0] == 2 and a[3][1] == 2 and a[3][2] == 2 and a[3][3] == 2 :
+        return True
+    
+    
     pass
 
 def verif_diagonal():
